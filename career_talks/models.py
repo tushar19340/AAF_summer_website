1403476c892aab1d96be4a2ce33ae1abac8e026f
Vaibhav Soni <vaibhavsoni280801@gmail.com>
2021-06-18 23:55:12 +0530

    Added some models in app

--- career_talks/models.py
+++ career_talks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+def Category():
+    name=models.CharField(max_length=50)
+
+def Item():
+    title=models.CharField(max_length=200)
+    sub_text=models.CharField(max_length=1000)
+    video_link=models.CharField(max_length=100)
+    image_url=models.CharField(max_length=100)
--- contests/models.py
+++ contests/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+def Item():
+    title=models.CharField(max_length=200)
+    description=models.CharField(max_length=1000)
+    start_date=models.DateField()
+    end_date=models.DateField()
+
+def Category():
+    name=models.CharField(max_length=100)
+
